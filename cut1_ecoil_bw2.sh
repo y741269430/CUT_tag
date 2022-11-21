b641ef8bd63b5604ed153bd94474dc5f8028d24f
y741269430 <56186383+y741269430@users.noreply.github.com>
2022-11-21 20:19:14 +0800

    Update cut1_ecoil_bw2.sh

--- cut1_ecoil_bw2.sh
+++ cut1_ecoil_bw2.sh
@@ -9,5 +9,5 @@ nohup bowtie2 --end-to-end --very-sensitive --no-overlap --no-dovetail --no-mixe
 -I 10 -X 700 -p 8 -x ${spikeInRef} \
 -1 ./raw/BL6-TG-CUT-${i}_1.fq.gz \
 -2 ./raw/BL6-TG-CUT-${i}_2.fq.gz \
--S ./bam/${i}_ecoil_bowtie2.sam &> ./info/${i}_ecoil_bowtie2.txt &
+-S ./bam/${i}_ecoil_bowtie2.sam &> ./bowtie2_summary/${i}_ecoil_bowtie2.txt &
 done

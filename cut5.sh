64226a9f7e4fd3d29da23d1c08514419b552ee61
y741269430 <56186383+y741269430@users.noreply.github.com>
2022-11-22 15:38:28 +0800

    Create cut5.sh

---
+++ cut5.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## Spike-in calibration ##
+
+cat filenames | while read i; 
+do
+bedtools genomecov -bg -i ./bam/${i}_mm10_bowtie2.fragments.bed \
+-g /home/yangjiajun/downloads/genome/mm10_GRCm38/ucsc_fa/mm10.chrom.sizes > ./bedgraph/${i}_mm10_bowtie2.fragments.normalized.bedgraph &
+done

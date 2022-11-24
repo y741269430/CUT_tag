76c89163ba0c97779be1c2256e71ba5a5c6a4974
y741269430 <56186383+y741269430@users.noreply.github.com>
2022-11-24 09:49:19 +0800

    Update cut5_bedgraph.sh

--- cut5_bedgraph.sh
+++ cut5_bedgraph.sh
@@ -3,6 +3,6 @@
 
 cat filenames | while read i; 
 do
-bedtools genomecov -bg -i ./bam/${i}_mm10_bowtie2.fragments.bed \
+bedtools genomecov -bg -i ./bed/${i}_mm10_bowtie2.fragments.bed \
 -g /home/yangjiajun/downloads/genome/mm10_GRCm38/ucsc_fa/mm10.chrom.sizes > ./bedgraph/${i}_mm10_bowtie2.fragments.normalized.bedgraph &
 done

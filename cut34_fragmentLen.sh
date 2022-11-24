f0b15a9f8de5f378a4870fa0b514a8266ba51c51
y741269430 <56186383+y741269430@users.noreply.github.com>
2022-11-24 13:40:26 +0800

    Update cut34_fragmentLen.sh

--- cut34_fragmentLen.sh
+++ cut34_fragmentLen.sh
@@ -4,7 +4,7 @@ cat filenames | while read i;
 do
 ## Extract the 9th column from the alignment sam file which is the fragment length
 nohup samtools view \
--F 0x04 ./bam/${i}_mm10_bowtie2.sam | awk \
+-@ 4 -F 0x04 ./bam/${i}_mm10_bowtie2.sam | awk \
 -F'\t' 'function abs(x){return ((x < 0.0) ? -x : x)} {print abs($9)}' | sort | uniq -c | awk \
 -v OFS="\t" '{print $2, $1/2}' > ./fragmentLen/${i}_fragmentLen.txt &
 

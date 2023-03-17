77370cd42fbc817a0f52852a795c2b787748ab6c
y741269430 <56186383+y741269430@users.noreply.github.com>
2023-03-17 09:18:21 +0800

    Delete cut5_bedgraph.sh

--- cut5_bedgraph.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## Spike-in calibration ##
-
-cat filenames | while read i; 
-do
-bedtools genomecov -bg -i ./bed/${i}_mm10_bowtie2.fragments.bed \
--g /home/yangjiajun/downloads/genome/mm10_GRCm38/ucsc_fa/mm10.chrom.sizes > ./bedgraph/${i}_mm10_bowtie2.fragments.normalized.bedgraph &
-done
